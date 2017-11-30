528ce20129c80bae13835b2d6e9e32e0444e0639
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2017-11-30 13:35:34 -0500

    change version number

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='xesmf',
-      version='0.0.1',
+      version='0.1.0',
       description='use ESMF regridding through xarray',
       url='http://github.com/jiaweizhuang/xesmf',
       author='Jiawei Zhuang',
       author_email='jiaweizhuang@g.harvard.edu',
       license='MIT',
       packages=['xesmf'],
-      install_requires=['esmpy','xarray','numpy','matplotlib','cartopy']
+      install_requires=['esmpy','xarray','numpy','scipy']
       )

5a0f8936c7793ae58dad6e8238ee4ce85c955b86
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2017-12-14 15:53:25 -0500

    Add test data

--- xesmf/__init__.py
+++ xesmf/__init__.py
@@ -1,1 +1,2 @@
 from . import util
+from . import testdata
---
+++ xesmf/testdata.py
@@ -0,0 +1,42 @@
+"""
+Standard test data for regridding benchmark.
+
+Data field originally proposed by Jones (1999).
+Ullrich et al. (2009) contain the figures.
+
+Reference:
+Jones, P. W. (1999). First-and second-order conservative remapping schemes for
+grids in spherical coordinates. Monthly Weather Review, 127(9), 2204-2210.
+
+Ullrich, P. A., Lauritzen, P. H., & Jablonowski, C. (2009). Geometrically exact
+conservative remapping (GECoRe): regular latitude–longitude and cubed-sphere
+grids. Monthly Weather Review, 137(6), 1721-1741.
+"""
+
+import numpy as np
+
+
+def wave_smooth(lon, lat):
+    '''
+    Spherical harmonic with low frequency
+    Y_2^2 = 2 + \cos^2(\theta) \cos(2 \phi)
+    '''
+    # degree to radius, make a copy
+    lat = lat/180.0*np.pi
+    lon = lon/180.0*np.pi
+
+    f = 2 + np.cos(lat)**2 * np.cos(2*lon)
+    return f
+
+
+def wave_sharp(lon, lat):
+    '''
+    Spherical harmonic with high frequency
+    Y_32^16 = 2 + \sin^16(2 \theta) \cos(16 \phi)
+    '''
+    # degree to radius, make a copy
+    lat = lat/180.0*np.pi
+    lon = lon/180.0*np.pi
+
+    f = 2 + np.sin(2*lat)**16 * np.cos(16*lon)
+    return f

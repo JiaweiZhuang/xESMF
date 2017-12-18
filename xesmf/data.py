42d68209080af95a25e00632dc2712de8fc59efd
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2017-12-17 22:04:27 -0500

    Use equations and references

--- xesmf/data.py
+++ xesmf/data.py
@@ -1,16 +1,5 @@
 """
 Standard test data for regridding benchmark.
-
-Data field originally proposed by Jones (1999).
-Ullrich et al. (2009) contain the figures.
-
-Reference:
-Jones, P. W. (1999). First-and second-order conservative remapping schemes for
-grids in spherical coordinates. Monthly Weather Review, 127(9), 2204-2210.
-
-Ullrich, P. A., Lauritzen, P. H., & Jablonowski, C. (2009). Geometrically exact
-conservative remapping (GECoRe): regular latitude–longitude and cubed-sphere
-grids. Monthly Weather Review, 137(6), 1721-1741.
 """
 
 import numpy as np
@@ -18,8 +7,7 @@ import numpy as np
 
 def wave_smooth(lon, lat):
     '''
-    Spherical harmonic with low frequency
-    Y_2^2 = 2 + \cos^2(\theta) \cos(2 \phi)
+    Spherical harmonic with low frequency.
 
     Parameters
     ----------
@@ -30,6 +18,23 @@ def wave_smooth(lon, lat):
     -------
     f : 2D numpy array or xarray DataArray depending on input
         2D wave field
+
+    Notes
+    -------
+    Equation from [1]_ [2]_:
+
+    .. math:: Y_2^2 = 2 + \cos^2(\\theta) \cos(2 \phi)
+
+    References
+    ----------
+    .. [1] Jones, P. W. (1999). First-and second-order conservative remapping
+       schemes for grids in spherical coordinates. Monthly Weather Review,
+       127(9), 2204-2210.
+
+    .. [2] Ullrich, P. A., Lauritzen, P. H., & Jablonowski, C. (2009).
+       Geometrically exact conservative remapping (GECoRe): regular
+       latitude–longitude and cubed-sphere grids. Monthly Weather Review,
+       137(6), 1721-1741.
     '''
     # degree to radius, make a copy
     lat = lat/180.0*np.pi

472edcddfabefbfa57948853a6dd5a746c2a81c6
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2017-12-31 20:49:40 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 DISTNAME = 'xesmf'
 DESCRIPTION = "Universal Regridder for Geospatial Data"
 AUTHOR = 'Jiawei Zhuang'

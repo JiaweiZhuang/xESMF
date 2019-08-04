50477f7e41da4342cd7b75d71f543f82f15565c5
jiaweizhuang <jiaweizhuang@g.harvard.edu>
2019-08-03 17:49:03 -0700

    bump version number for new release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DISTNAME = 'xesmf'
 DESCRIPTION = "Universal Regridder for Geospatial Data"
 AUTHOR = 'Jiawei Zhuang'
--- xesmf/__init__.py
+++ xesmf/__init__.py
@@ -1,3 +1,4 @@
+__version__='0.1.2'
 from . import util
 from . import data
 from . frontend import Regridder

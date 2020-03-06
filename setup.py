2d2e365065711337286404e36fcb23c0def9eba8
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2020-03-06 14:55:21 -0500

    bump version to 0.3.0 for locstream capability

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = '0.2.2'
+VERSION = '0.3.0'
 DISTNAME = 'xesmf'
 DESCRIPTION = "Universal Regridder for Geospatial Data"
 AUTHOR = 'Jiawei Zhuang'

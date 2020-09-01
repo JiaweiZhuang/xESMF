d6f0cd820ef4062a996f47fbe6c1b6de0238757f
Raphael Dussin <raphael.dussin@gmail.com>
2020-09-01 13:16:08 -0400

    new funcs need esmpy>=8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if on_rtd:
     INSTALL_REQUIRES = []
 else:
-    INSTALL_REQUIRES = ['esmpy', 'xarray', 'numpy', 'scipy']
+    INSTALL_REQUIRES = ['esmpy>=8.0.0', 'xarray', 'numpy', 'scipy']
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',

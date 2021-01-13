8bff01b0a281974b680621fbd64ed4beff562d4f
Stephane Raynaud <stephane.raynaud@shom.fr>
2021-01-13 15:38:22 +0100

    Make adapative masking less sensitive to floats

--- xesmf/frontend.py
+++ xesmf/frontend.py
@@ -440,7 +440,7 @@ class BaseRegridder(object):
         if adaptative_masking:
             outvalid = apply_weights(
                 weights, (~inmask).astype('d'), shape_in, shape_out)
-            bad = outvalid == 0
+            bad = np.isclose(outvalid, 0)
             outvalid[bad] = 1
             outdata = xr.where(bad, np.nan, outdata / outvalid)
 

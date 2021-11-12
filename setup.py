467b1e7ca9a1b8a65a080e28e7ec4a7c45855c0a
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2021-11-12 13:54:19 -0500

    unpin shapely (#134)
    Remove a pin added in #128.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ else:
         'esmpy>=8.0.0',
         'xarray>=0.16.2',
         'numpy>=1.16',
-        'shapely<=1.7',
+        'shapely',
         'cf-xarray>=0.5.1',
         'sparse',
     ]
